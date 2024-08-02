e41bd9a43e8fd61cd3e4c004a842a19e086647e7
Mwanda Chipongo <mwandachips00@gmail.com>
2024-08-02 15:48:02 +0200

    Initial Commit

---
+++ Memoize/MemoizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoizeApp.swift
+//  Memoize
+//
+//  Created by Mwanda Chipongo on 02/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct MemoizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
