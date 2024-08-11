923e5b99cf787469202048d641ea272091db9a0f
Mwanda Chipongo <mwandachips00@gmail.com>
2024-08-11 15:39:48 +0200

    Initial Commit Card View isFaceUp

--- Memoize/ContentView.swift
+++ Memoize/ContentView.swift
@@ -10,14 +10,48 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, Nigga!")
+            Cardview(isFaceUp: true)
+            Cardview()
+            Cardview()
+            Cardview()
         }
+        .foregroundColor(.orange)
         .padding()
     }
 }
+
+
+struct Cardview: View {
+    var isFaceUp: Bool = false
+    var body: some View {
+        ZStack(content: {
+            if isFaceUp {
+                RoundedRectangle(cornerRadius: 12)
+                    .foregroundColor(.white)
+                RoundedRectangle(cornerRadius: 12)
+                    .strokeBorder(lineWidth: 2)
+                Text("💀").font(.largeTitle)
+            } else {
+                RoundedRectangle(cornerRadius: 12)
+                RoundedRectangle(cornerRadius: 12)
+                    .strokeBorder(lineWidth: 2)
+            }
+        })
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 #Preview {
     ContentView()
