61beed49934ca5bc14ebd5ee25b0acc639499c78
Mwanda Chipongo <mwandachips00@gmail.com>
2024-08-12 10:15:36 +0200

    Annotation and a few edits

--- Memoize/ContentView.swift
+++ Memoize/ContentView.swift
@@ -12,7 +12,7 @@ struct ContentView: View {
         VStack {
             Cardview(isFaceUp: true)
             Cardview()
-            Cardview()
+            Cardview(isFaceUp: true)
             Cardview()
         }
         .foregroundColor(.orange)
@@ -22,21 +22,34 @@ struct ContentView: View {
 
 
 struct Cardview: View {
-    var isFaceUp: Bool = false
+    // Setting @State before the var causes it to create a pointer to isFaceUp
+    // Since a view is immutable by default, this allows values in a view to be changed
+    // because the pointer isn't changing, the value it is pointing at is.
+    @State var isFaceUp: Bool = false
+    // Default value is set to prevent providing a value
+    // every single time.
+    // If a let is used, the user can only set it once
+    // Otherwise it can be swapped
+    // Always start off with it being immutable
+    
     var body: some View {
-        ZStack(content: {
+        ZStack{
+            // Type inference used here, don't have to write Rounded Rectangle twice
+            let base = RoundedRectangle(cornerRadius: 12)
             if isFaceUp {
-                RoundedRectangle(cornerRadius: 12)
-                    .foregroundColor(.white)
-                RoundedRectangle(cornerRadius: 12)
-                    .strokeBorder(lineWidth: 2)
+                base.fill(.white)
+                base.strokeBorder(lineWidth: 2)
                 Text("💀").font(.largeTitle)
             } else {
-                RoundedRectangle(cornerRadius: 12)
-                RoundedRectangle(cornerRadius: 12)
-                    .strokeBorder(lineWidth: 2)
+                base.fill()
             }
-        })
+        }
+        .onTapGesture {
+            //isFaceUp = !isFaceUp
+            // this throws and error cause views are immutable
+            // The idiomatic Swift way to change this would be to use .toggle()
+            isFaceUp.toggle()
+        }
     }
 }
 
