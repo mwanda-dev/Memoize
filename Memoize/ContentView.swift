2d6e0a848838469c76d48cb1fc736540b309bb70
Mwanda Chipongo <mwandachips00@gmail.com>
2024-08-19 09:05:18 +0200

    Function written for the card modifiers

--- Memoize/ContentView.swift
+++ Memoize/ContentView.swift
@@ -33,24 +33,27 @@ struct ContentView: View {
         .imageScale(.large)
     }
     
-    var cardRemover: some View {
+    // Internal vs external parameter names
+    // When we pass in an argument to a constructor of something, we have to say system name calling
+    // For symbol the first arg with a 'by' is an external name while just offset is internal to the function
+    // symbol is both the internal and external name which is used by both the caller and implementor
+    func cardCountAdjuster(by offset: Int, symbol: String) -> some View {
         Button(action: {
-            if cardCount > 1 {
-                cardCount -= 1
-            }
+            // Here 'offset' is used, not by offset
+            // The caller will use 'by offset'
+            cardCount += offset
         }, label: {
-            Image(systemName: "rectangle.stack.badge.minus")
+            Image(systemName: symbol)
         })
+        .disabled(cardCount + offset < 1 || cardCount + offset > emojis.count)
+    }
+    
+    var cardRemover: some View {
+        cardCountAdjuster(by: -1, symbol: "rectangle.stack.badge.minus")
     }
     
     var cardAdder: some View {
-        Button(action: {
-            if cardCount < emojis.count {
-                cardCount += 1
-            }
-        }, label: {
-            Image(systemName: "rectangle.stack.badge.plus")
-        })
+        cardCountAdjuster(by: +1, symbol: "rectangle.stack.badge.plus")
     }
     
     var cards: some View {
