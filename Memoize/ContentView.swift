03982eda6cd88aeba8d3e4525ec65fde1454b469
Mwanda Chipongo <mwandachips00@gmail.com>
2024-08-19 08:38:07 +0200

    Button guards added

--- Memoize/ContentView.swift
+++ Memoize/ContentView.swift
@@ -30,7 +30,9 @@ struct ContentView: View {
             
             HStack {
                 Button(action: {
-                  cardCount -= 1
+                    if cardCount > 1 {
+                        cardCount -= 1
+                    }
                 }, label: {
                     Image(systemName: "rectangle.stack.badge.minus")
                 })
@@ -38,7 +40,9 @@ struct ContentView: View {
                 Spacer()
                 
                 Button(action: {
-                  cardCount += 1
+                    if cardCount < emojis.count {
+                        cardCount += 1
+                    }
                 }, label: {
                     Image(systemName: "rectangle.stack.badge.plus")
                 })
